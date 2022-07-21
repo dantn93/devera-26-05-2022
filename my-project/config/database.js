4ff17b6351ef7e0780f9a2be1e4823265bcda98e
Dan <dantn93@gmail.com>
2022-07-21 12:59:16 +0700

    init

---
+++ my-project/config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'database-1.c9qrogir5qab.ap-southeast-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'devera'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'jgspNO5iEBrZdzseAea7'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
